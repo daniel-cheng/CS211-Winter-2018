9a6debdc8fca6c42025d4dd25ef33a895d22e0ee
Qiyu Zhao <zhaoqiyu1992@gmail.com>
2018-01-23 23:57:03 -0800

    Part2 Report

--- laplacian.m
+++ laplacian.m
@@ -1,25 +1,20 @@
-input = imread('img\text.jpg');
+input = imread('img\\text.jpg');
 origin = input;
 filter = [1/4 1/4; 1/4 1/4];
 for i = 1:8
     result = conv2(input, filter, 'valid'); %do the convolution,
     size(result)
     smImg = result(1:2:end, 1:2:end); %subsampling
-    origin = double(origin(1:2:end, 1:2:end));
+    origin = double(origin(1:2:end, 1:2:end)); %subsampling original image for subtraction
     
-    diff = origin - smImg;
-    if i < 8
-        display = imresize(diff, 2^(i), 'bilinear');
-
-        %display = interp2(origin, i);
-        %display = interp2(diff, i);        
-    else
-        %display = imresize(smImg, 256);
-        display = repmat(diff, 256);
-    end
-    mean(mean(display))
-    figure
+    diff = origin - smImg; %subtract
+    display = imresize(diff, 2^(i), 'bilinear'); %resizing
+    %creating output image file
+    f = figure;
+    filename = sprintf('output\\CARTOON_laplacial_fig%u.jpg', i);
+    
     imshow(mat2gray(display));
+    saveas(f, filename);
     
-    input = smImg;    
+    input = smImg;     %update input
 end
