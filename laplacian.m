c6c33f226971f72ed6c291c2c9c93b370ff38532
Qiyu Zhao <zhaoqiyu1992@gmail.com>
2018-01-23 17:17:59 -0800

    Code for part 2

--- laplacian.m
+++ laplacian.m
@@ -1,47 +1,25 @@
-input = imread('img\CARTOON.jpg');
+input = imread('img\text.jpg');
+origin = input;
 filter = [1/4 1/4; 1/4 1/4];
-%filter = [-1/8 -1/8 -1/8; -1/8 1 -1/8; -1/8 -1/8 -1/8];
-figure;
-%result = imresize(result, 0.5,'Method', 'bilinear');
-result = conv2(input, filter, 'same');
-imshow(imresize(mat2gray(result), 2, 'Method', 'bilinear'));
-result = imresize(result, 0.5,'Method', 'bilinear'); 
-result = conv2(result, filter, 'same');
-figure;
-imshow(imresize(mat2gray(result), 4, 'Method', 'bilinear'));
-result = imresize(result, 0.5,'Method', 'bilinear'); 
-result = conv2(result, filter, 'same');
+for i = 1:8
+    result = conv2(input, filter, 'valid'); %do the convolution,
+    size(result)
+    smImg = result(1:2:end, 1:2:end); %subsampling
+    origin = double(origin(1:2:end, 1:2:end));
+    
+    diff = origin - smImg;
+    if i < 8
+        display = imresize(diff, 2^(i), 'bilinear');
 
-figure;
-imshow(imresize(mat2gray(result), 8, 'Method', 'bilinear'));
-result = imresize(result, 0.5,'Method', 'bilinear'); 
-
-result = conv2(result, filter, 'same');
-
-figure;
-imshow(imresize(mat2gray(result), 16, 'Method', 'bilinear'));
-result = imresize(result, 0.5,'Method', 'bilinear'); 
-
-result = conv2(result, filter, 'same');
-
-figure;
-imshow(imresize(mat2gray(result), 32, 'Method', 'bilinear'));
-result = imresize(result, 0.5,'Method', 'bilinear'); 
-
-result = conv2(result, filter, 'same');
-
-figure;
-imshow(imresize(mat2gray(result), 64, 'Method', 'bilinear'));
-result = imresize(result, 0.5,'Method', 'bilinear'); 
-
-result = conv2(result, filter, 'same');
-
-figure;
-imshow(imresize(mat2gray(result), 128, 'Method', 'bilinear'));
-result = imresize(result, 0.5,'Method', 'bilinear'); 
-
-result = conv2(result, filter, 'same');
-
-figure;
-imshow(imresize(mat2gray(result), 256, 'Method', 'bilinear'));
-
+        %display = interp2(origin, i);
+        %display = interp2(diff, i);        
+    else
+        %display = imresize(smImg, 256);
+        display = repmat(diff, 256);
+    end
+    mean(mean(display))
+    figure
+    imshow(mat2gray(display));
+    
+    input = smImg;    
+end
