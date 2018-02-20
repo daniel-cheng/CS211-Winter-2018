f3cea36167147fa76b0b7efa297f2946fdde6506
Daniel Cheng <dcheng334@gmail.com>
2018-02-20 01:18:58 -0800

    CHG: Committing working version of part 2.

--- hw3/code/calculate_camera_matrix.m
+++ hw3/code/calculate_camera_matrix.m
@@ -7,21 +7,21 @@ function [C_matrix, u, v] = calculate_camera_matrix(image, u, v, p)
     %[u, v] = getpts;
     %disp(u)
     %disp(v)
-    syms r1x r1y r1z r1w r2x r2y r2z r2w r3x r3y r3z r3w 
-    eqn1 = u(1) * (r3x * p{1}(1) + r3y * p{1}(2) + r3z * p{1}(3) + r3w * p{1}(4)) - (r1x * p{1}(1) + r1y * p{1}(2) + r1z * p{1}(3) + r1w * p{1}(4)) == 0;
-    eqn2 = v(1) * (r3x * p{1}(1) + r3y * p{1}(2) + r3z * p{1}(3) + r3w * p{1}(4)) - (r2x * p{1}(1) + r2y * p{1}(2) + r2z * p{1}(3) + r2w * p{1}(4)) == 0;
-    eqn3 = u(2) * (r3x * p{2}(1) + r3y * p{2}(2) + r3z * p{2}(3) + r3w * p{2}(4)) - (r1x * p{2}(1) + r1y * p{2}(2) + r1z * p{2}(3) + r1w * p{2}(4)) == 0;
-    eqn4 = v(2) * (r3x * p{2}(1) + r3y * p{2}(2) + r3z * p{2}(3) + r3w * p{2}(4)) - (r2x * p{2}(1) + r2y * p{2}(2) + r2z * p{2}(3) + r2w * p{2}(4)) == 0;
-    eqn5 = u(3) * (r3x * p{3}(1) + r3y * p{3}(2) + r3z * p{3}(3) + r3w * p{3}(4)) - (r1x * p{3}(1) + r1y * p{3}(2) + r1z * p{3}(3) + r1w * p{3}(4)) == 0;
-    eqn6 = v(3) * (r3x * p{3}(1) + r3y * p{3}(2) + r3z * p{3}(3) + r3w * p{3}(4)) - (r2x * p{3}(1) + r2y * p{3}(2) + r2z * p{3}(3) + r2w * p{3}(4)) == 0;
-    eqn7 = u(4) * (r3x * p{4}(1) + r3y * p{4}(2) + r3z * p{4}(3) + r3w * p{4}(4)) - (r1x * p{4}(1) + r1y * p{4}(2) + r1z * p{4}(3) + r1w * p{4}(4)) == 0;
-    eqn8 = v(4) * (r3x * p{4}(1) + r3y * p{4}(2) + r3z * p{4}(3) + r3w * p{4}(4)) - (r2x * p{4}(1) + r2y * p{4}(2) + r2z * p{4}(3) + r2w * p{4}(4)) == 0;
-    eqn9 = u(5) * (r3x * p{5}(1) + r3y * p{5}(2) + r3z * p{5}(3) + r3w * p{5}(4)) - (r1x * p{5}(1) + r1y * p{5}(2) + r1z * p{5}(3) + r1w * p{5}(4)) == 0;
-    eqn10 = v(5) * (r3x * p{5}(1) + r3y * p{5}(2) + r3z * p{5}(3) + r3w * p{5}(4)) - (r2x * p{5}(1) + r2y * p{5}(2) + r2z * p{5}(3) + r2w * p{5}(4)) == 0;
-    eqn11 = u(6) * (r3x * p{6}(1) + r3y * p{6}(2) + r3z * p{6}(3) + r3w * p{6}(4)) - (r1x * p{6}(1) + r1y * p{6}(2) + r1z * p{6}(3) + r1w * p{6}(4)) == 0;
-    eqn12 = v(6) * (r3x * p{6}(1) + r3y * p{6}(2) + r3z * p{6}(3) + r3w * p{6}(4)) - (r2x * p{6}(1) + r2y * p{6}(2) + r2z * p{6}(3) + r2w * p{6}(4)) == 0;
-    %sol = solve([eqn1, eqn2, eqn3, eqn4, eqn5, eqn6, eqn7, eqn8, eqn9, eqn10, eqn11, eqn12], [r1x, r1y, r1z, r1w, r2x, r2y, r2z, r2w, r3x, r3y, r3z, r3w]);
-    [A,B] = equationsToMatrix([eqn1, eqn2, eqn3, eqn4, eqn5, eqn6, eqn7, eqn8, eqn9, eqn10, eqn11, eqn12], [r1x, r1y, r1z, r1w, r2x, r2y, r2z, r2w, r3x, r3y, r3z, r3w]);
+    syms r1x r1y r1z r1w r2x r2y r2z r2w r3x r3y r3z 
+    eqn1 = u(1) * (r3x * p{1}(1) + r3y * p{1}(2) + r3z * p{1}(3) + 1) == (r1x * p{1}(1) + r1y * p{1}(2) + r1z * p{1}(3) + r1w);
+    eqn2 = v(1) * (r3x * p{1}(1) + r3y * p{1}(2) + r3z * p{1}(3) + 1) == (r2x * p{1}(1) + r2y * p{1}(2) + r2z * p{1}(3) + r2w);
+    eqn3 = u(2) * (r3x * p{2}(1) + r3y * p{2}(2) + r3z * p{2}(3) + 1) == (r1x * p{2}(1) + r1y * p{2}(2) + r1z * p{2}(3) + r1w);
+    eqn4 = v(2) * (r3x * p{2}(1) + r3y * p{2}(2) + r3z * p{2}(3) + 1) == (r2x * p{2}(1) + r2y * p{2}(2) + r2z * p{2}(3) + r2w);
+    eqn5 = u(3) * (r3x * p{3}(1) + r3y * p{3}(2) + r3z * p{3}(3) + 1) == (r1x * p{3}(1) + r1y * p{3}(2) + r1z * p{3}(3) + r1w);
+    eqn6 = v(3) * (r3x * p{3}(1) + r3y * p{3}(2) + r3z * p{3}(3) + 1) == (r2x * p{3}(1) + r2y * p{3}(2) + r2z * p{3}(3) + r2w);
+    eqn7 = u(4) * (r3x * p{4}(1) + r3y * p{4}(2) + r3z * p{4}(3) + 1) == (r1x * p{4}(1) + r1y * p{4}(2) + r1z * p{4}(3) + r1w);
+    eqn8 = v(4) * (r3x * p{4}(1) + r3y * p{4}(2) + r3z * p{4}(3) + 1) == (r2x * p{4}(1) + r2y * p{4}(2) + r2z * p{4}(3) + r2w);
+    eqn9 = u(5) * (r3x * p{5}(1) + r3y * p{5}(2) + r3z * p{5}(3) + 1) == (r1x * p{5}(1) + r1y * p{5}(2) + r1z * p{5}(3) + r1w);
+    eqn10 = v(5) * (r3x * p{5}(1) + r3y * p{5}(2) + r3z * p{5}(3) + 1) == (r2x * p{5}(1) + r2y * p{5}(2) + r2z * p{5}(3) + r2w);
+    eqn11 = u(6) * (r3x * p{6}(1) + r3y * p{6}(2) + r3z * p{6}(3) + 1) == (r1x * p{6}(1) + r1y * p{6}(2) + r1z * p{6}(3) + r1w);
+    %eqn12 = v(6) * (r3x * p{6}(1) + r3y * p{6}(2) + r3z * p{6}(3) + 1) == (r2x * p{6}(1) + r2y * p{6}(2) + r2z * p{6}(3) + r2w);
+    %sol = solve([eqn1, eqn2, eqn3, eqn4, eqn5, eqn6, eqn7, eqn8, eqn9, eqn10, eqn11, eqn12], [r1x, r1y, r1z, r1w, r2x, r2y, r2z, r2w, r3x, r3y, r3z, 1]);
+    [A,B] = equationsToMatrix([eqn1, eqn2, eqn3, eqn4, eqn5, eqn6, eqn7, eqn8, eqn9, eqn10, eqn11], [r1x, r1y, r1z, r1w, r2x, r2y, r2z, r2w, r3x, r3y, r3z]);
     X = linsolve(A,B);
-    C_matrix = [X(1) X(2) X(3) X(4); X(5) X(6) X(7) X(8); X(9) X(10) X(11) X(12)];
+    C_matrix = eval([X(1) X(2) X(3) X(4); X(5) X(6) X(7) X(8); X(9) X(10) X(11) 1]);
 end
--- hw3/code/part2.m
+++ hw3/code/part2.m
@@ -9,10 +9,10 @@ function [C_matrices, u, v] = part2(u, v)
     points = cell(6, 1);
     points{1} = [16 16 48 1];
     points{2} = [48 16 48 1];
-    points{3} = [0 48 67 1];
+    points{3} = [0.01 48 67 1];
     points{4} = [32 48 67 1];
     points{5} = [32 80 67 1];
-    points{6} = [0 80 67 1];
+    points{6} = [0.01 80 67 1];
     %Uncomment the below 2 lines if you want to regenerate these
     %u = cell(number_of_files, 1); 
     %v = cell(number_of_files, 1);
@@ -20,7 +20,7 @@ function [C_matrices, u, v] = part2(u, v)
        filename = image_files(i).name;
        images{i} = imread(['../data/' filename]);
        [C_matrices{i}, u{i}, v{i}] = calculate_camera_matrix(images{i}, u{i}, v{i}, points);
-       disp(['Camera calibraration matrix for image i = ' num2str(i) ': ']);
+       disp(['Camera calibraration matrix for image ' filename ' i = ' num2str(i) ': ']);
        disp(C_matrices{i});
        %[C_matrices{i}, u{i}, v{i}] = calculate_camera_matrix(images{i}, points);
     end
